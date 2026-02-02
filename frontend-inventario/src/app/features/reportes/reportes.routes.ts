3c592585aa35ef63e106855f6e446a37f9c36e9f
Alex Alba <kalexmiguelalba@gmail.com>
2026-02-01 22:28:02 -0500

    ok evide3ncias y backend y frontend

--- frontend-inventario/src/app/features/reportes/reportes.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ReporteInventarioComponent } from './pages/reporte-inventario/reporte-inventario.component';
-
-export const REPORTES_ROUTES: Routes = [
-  { path: 'inventario', component: ReporteInventarioComponent },
-];
